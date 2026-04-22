c7c196bf9dd05add2103910e3fb1dc1b57d1b734
Ossama <sabihiossama318@gmail.com>
2026-04-22 17:22:54 +0200

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
+
+int _evaluate_sqrt(int n, int i)
+{
+    if(i * i == n)
+        return(i);
+    else if(i * i > n)
+    return (-1);
+    else
+        return (_evaluate_sqrt(n, i + 1));
+}
+
+/**
+ * natural_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+    return (_evaluate_sqrt(n, 0));
+}
