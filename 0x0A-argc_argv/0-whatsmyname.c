10b346d5f361cf5c9dc8bf19211405f3e9b878a9
Ossama <sabihiossama318@gmail.com>
2026-04-28 19:48:27 +0100

    It aint what they call you, its what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print my name (function's name)
+ * @argc: first arg count
+ * @argv: second arg vector
+ * Return: succes compilation ok
+ */
+
+ int main(int argc, char *argv[])
+ {
+    (void)argc;
+    int i = 0;
+    printf("%s\n", argv[i]);
+
+    return (0);
+ }
