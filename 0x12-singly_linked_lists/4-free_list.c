dd3e3dcfa76a1eb19064f6ef76fc109d5e233e60
Ossama <sabihiossama318@gmail.com>
2026-08-31 12:26:29 +0100

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: the head of the list.
+ * 
+ * Return: void.
+ */
+
+void free_list(list_t *head)
+{
+    list_t *t;
+
+    if (head == NULL)
+        return;
+    
+    while (head != NULL)
+    {
+        t = head->next;
+        free(head->str);
+        free(head);
+        head = t;
+    }
+}
